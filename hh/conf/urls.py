debbc3098ac5cc57fe4104d0d6ce4ccd27707dfa
anatolytsi <snipl33t@gmail.com>
2022-01-03 18:22:22 +0100

    added vacancies url

--- hh/conf/urls.py
+++ hh/conf/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from vacancies.views import VacancyList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vacancies/', VacancyList.as_view())
 ]
