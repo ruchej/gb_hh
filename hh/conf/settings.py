8768e165fe5f313dfdb27c32927ef44c97459770
Aleksandr <lvar-8@ya.ru>
2022-01-05 00:18:44 +0300

    Внёс измениня в настройки для приложения аккаунтов

--- hh/conf/settings.py
+++ hh/conf/settings.py
@@ -34,61 +34,59 @@ ALLOWED_HOSTS = local_settings.ALLOWED_HOSTS
 
 INSTALLED_APPS = [
     # builtin
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # installed
-
     # custom
-    'conf',
-    'accounts.apps.AccountsConfig',
-    'blog.apps.BlogConfig',
-    'resumes.apps.ResumesConfig',
-    'vacancies.apps.VacanciesConfig',
+    "conf",
+    "accounts.apps.AccountsConfig",
+    "blog.apps.BlogConfig",
+    "resumes.apps.ResumesConfig",
+    "vacancies.apps.VacanciesConfig",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'conf.urls'
+ROOT_URLCONF = "conf.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'conf.wsgi.application'
+WSGI_APPLICATION = "conf.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -98,26 +96,28 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
+AUTH_USER_MODEL = "accounts.Account"
+LOGIN_REDIRECT_URL = "/accounts/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -127,94 +127,95 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath('static')
-
+STATIC_URL = "/static/"
+STATICFILES_FINDERS = ["django.contrib.staticfiles.finders.FileSystemFinder",
+                       "django.contrib.staticfiles.finders.AppDirectoriesFinder"]
+STATIC_ROOT = BASE_DIR.joinpath("static")
 
 # Media files
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR.joinpath("media")
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 try:
-    os.makedirs(BASE_DIR.joinpath('logs'))    # make "logs" directory if not exists
+    os.makedirs(BASE_DIR.joinpath("logs"))  # make "logs" directory if not exists
 except FileExistsError:
     pass
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'default': {
-            'format': '{levelname}, {asctime}, {module}, {process:d}/{thread:d} - {message}',
-            'style': '{',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "{levelname}, {asctime}, {module}, {process:d}/{thread:d} - {message}",
+            "style": "{",
         },
     },
-    'handlers': {
-        'django_server': {
-            'level': 'WARNING',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': f'{BASE_DIR.joinpath("logs").joinpath("django_server.log")}',
-            'maxBytes': 1024 * 1024 * 5,
-            'backupCount': 5,
-            'formatter': 'default',
+    "handlers": {
+        "django_server": {
+            "level": "WARNING",
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": f'{BASE_DIR.joinpath("logs").joinpath("django_server.log")}',
+            "maxBytes": 1024 * 1024 * 5,
+            "backupCount": 5,
+            "formatter": "default",
         },
-        'accounts_file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': f'{BASE_DIR.joinpath("logs").joinpath("accounts.log")}',
-            'formatter': 'default',
+        "accounts_file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": f'{BASE_DIR.joinpath("logs").joinpath("accounts.log")}',
+            "formatter": "default",
         },
-        'resumes_file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': f'{BASE_DIR.joinpath("logs").joinpath("resumes.log")}',
-            'formatter': 'default',
+        "resumes_file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": f'{BASE_DIR.joinpath("logs").joinpath("resumes.log")}',
+            "formatter": "default",
         },
-        'vacancies_file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': f'{BASE_DIR.joinpath("logs").joinpath("vacancies.log")}',
-            'formatter': 'default',
+        "vacancies_file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": f'{BASE_DIR.joinpath("logs").joinpath("vacancies.log")}',
+            "formatter": "default",
         },
-        'blog_file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': f'{BASE_DIR.joinpath("logs").joinpath("blog.log")}',
-            'formatter': 'default',
+        "blog_file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": f'{BASE_DIR.joinpath("logs").joinpath("blog.log")}',
+            "formatter": "default",
         },
     },
-    'loggers': {
-        'django.server': {
-            'handlers': ['django_server'],
-            'propagate': True,
+    "loggers": {
+        "django.server": {
+            "handlers": ["django_server"],
+            "propagate": True,
         },
-        'accounts': {
-            'handlers': ['accounts_file'],
-            'level': 'DEBUG',
-            'propagate': True,
+        "accounts": {
+            "handlers": ["accounts_file"],
+            "level": "DEBUG",
+            "propagate": True,
         },
-        'resumes': {
-            'handlers': ['resumes_file'],
-            'level': 'DEBUG',
-            'propagate': True,
+        "resumes": {
+            "handlers": ["resumes_file"],
+            "level": "DEBUG",
+            "propagate": True,
         },
-        'vacancies': {
-            'handlers': ['vacancies_file'],
-            'level': 'DEBUG',
-            'propagate': True,
+        "vacancies": {
+            "handlers": ["vacancies_file"],
+            "level": "DEBUG",
+            "propagate": True,
         },
-        'blog': {
-            'handlers': ['blog_file'],
-            'level': 'DEBUG',
-            'propagate': True,
+        "blog": {
+            "handlers": ["blog_file"],
+            "level": "DEBUG",
+            "propagate": True,
         },
     },
 }
