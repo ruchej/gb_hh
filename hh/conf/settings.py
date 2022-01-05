6046d3a12d6a18acace1f88fc1dbaf45147dd07e
anatolytsi <snipl33t@gmail.com>
2022-01-05 11:26:47 +0100

    added static support

--- hh/conf/settings.py
+++ hh/conf/settings.py
@@ -127,8 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath('static')
+STATIC_URL = '/templates/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
 
 
 # Media files
