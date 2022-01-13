dacd482a569a929f393325e4da967ab5f83d21de
nat0t <nataniel1987@gmail.com>
2022-01-13 19:56:46 +0300

    Работа с шаблонами: - создана индексная страница; - создан базовый шаблон; - проставлены ссылки в хедере; - базовый шаблон проброшен в остальные шаблоны.'

--- hh/conf/urls.py
+++ hh/conf/urls.py
@@ -18,7 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
+app_name = 'conf'
+
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('resumes/', include('resumes.urls', namespace='resumes')),
