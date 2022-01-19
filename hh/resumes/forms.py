fae09587b5cf488748f53a1068a2234e454cb020
nat0t <nataniel1987@gmail.com>
2022-01-19 18:04:38 +0300

    Добавлены контроллеры для CRUD-операций с резюме.

--- hh/resumes/forms.py
+++ hh/resumes/forms.py
@@ -4,25 +4,35 @@ from . import models
 
 
 class PersonalInfoForm(forms.ModelForm):
+    """Form for processing information about personal information in resume."""
+
     model = models.PersonalInfo
     fields = ('name', 'patronymic', 'surname', 'birthday', 'gender', 'location', 'relocation', 'business_trip')
 
 
-class ContanctsForm(forms.ModelForm):
+class ContactsForm(forms.ModelForm):
+    """Form for processing information about contacts in resume."""
+
     model = models.Contacts
     fields = ('phone', 'email', 'telegram')
 
 
 class PositionForm(forms.ModelForm):
+    """Form for processing information about position in resume."""
+
     model = models.Position
     fields = ('title', 'salary', 'employment', 'schedule')
 
 
-class ExperienceFprm(forms.ModelForm):
+class ExperienceForm(forms.ModelForm):
+    """Form for processing information about experience in resume."""
+
     model = models.Experience
     fields = ('skills', 'about', 'portfolio')
 
 
 class JobForm(forms.ModelForm):
+    """Form for processing information about jobs in resume."""
+
     model = models.Job
     fields = ('organization', 'start', 'end', 'location', 'site', 'scope', 'position', 'functions')
--- hh/resumes/views.py
+++ hh/resumes/views.py
@@ -1,14 +1,118 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from . import models
+from . import models, forms
 
 
 class ResumeListView(LoginRequiredMixin, ListView):
+    """View for getting list of all resumes."""
+
     model = models.Resume
     template_name = 'brief-list.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(ResumeListView, self).get_context_data()
-        context.update({'title': 'Резюме'})
-        return context
+
+class ResumeDetailView(LoginRequiredMixin, DetailView):
+    """View for getting detail info in resume."""
+
+    model = models.Resume
+    template_name = 'brief-detail.html'
+
+
+class ResumeCreateView(LoginRequiredMixin, CreateView):
+    """View for creating resume."""
+    model = models.Resume
+    template_name = 'brief-create.html'
+
+
+class ResumeUpdateView(LoginRequiredMixin, UpdateView):
+    """View for updating resume."""
+
+    model = models.Resume
+    template_name = 'brief-update.html'
+
+
+class ResumeDeleteView(LoginRequiredMixin, DeleteView):
+    """View for deleting resume."""
+
+    model = models.Resume
+    template_name = 'brief-delete.html'
+
+
+class PersonalInfoDetailView(LoginRequiredMixin, DetailView):
+    """View for getting detail information about personal info in resume."""
+
+    model = models.PersonalInfo
+    form_class = forms.PersonalInfoForm
+    template_name = 'personal-info-detail.html'
+
+
+class PersonalInfoUpdateView(LoginRequiredMixin, UpdateView):
+    """View for updating personal info in resume."""
+
+    model = models.PersonalInfo
+    form_class = forms.PersonalInfoForm
+    template_name = 'personal-info-update.html'
+
+
+class ContactsDetailView(LoginRequiredMixin, DetailView):
+    """View for getting detail information about contacts in resume."""
+
+    model = models.Contacts
+    form_class = forms.ContactsForm
+    template_name = 'contacts-detail.html'
+
+
+class ContactsUpdateView(LoginRequiredMixin, UpdateView):
+    """View for updating contacts in resume."""
+
+    model = models.Contacts
+    form_class = forms.ContactsForm
+    template_name = 'contacts-update.html'
+
+
+class PositionDetailView(LoginRequiredMixin, DetailView):
+    """View for getting detail information about position in resume."""
+
+    model = models.Position
+    form_class = forms.PositionForm
+    template_name = 'position-detail.html'
+
+
+class PositionUpdateView(LoginRequiredMixin, UpdateView):
+    """View for updating position in resume."""
+
+    model = models.Position
+    form_class = forms.PositionForm
+    template_name = 'position-update.html'
+
+
+class ExperienceDetailView(LoginRequiredMixin, DetailView):
+    """View for getting detail information about experience in resume."""
+
+    model = models.Experience
+    form_class = forms.ExperienceForm
+    template_name = 'experience-detail.html'
+
+
+class ExperienceUpdateView(LoginRequiredMixin, UpdateView):
+    """View for updating experience in resume."""
+
+    model = models.Experience
+    form_class = forms.ExperienceForm
+    template_name = 'experience-update.html'
+
+
+class JobDetailView(LoginRequiredMixin, DetailView):
+    """View for getting detail information about jobs in resume."""
+
+    model = models.Job
+    form_class = forms.JobForm
+    template_name = 'job-detail.html'
+
+
+class JobUpdateView(LoginRequiredMixin, UpdateView):
+    """View for updating jobs in resume."""
+
+    model = models.Job
+    form_class = forms.JobForm
+    template_name = 'job-update.html'
