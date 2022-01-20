92d8ecb0efd97cda5a86c6d74e0b93bde8656f2d
nat0t <nataniel1987@gmail.com>
2022-01-20 19:47:04 +0300

    Исправлены формы resumes.

--- hh/resumes/forms.py
+++ hh/resumes/forms.py
@@ -5,34 +5,38 @@ from . import models
 
 class PersonalInfoForm(forms.ModelForm):
     """Form for processing information about personal information in resume."""
-
-    model = models.PersonalInfo
-    fields = ('name', 'patronymic', 'surname', 'birthday', 'gender', 'location', 'relocation', 'business_trip')
+    class Meta:
+        model = models.PersonalInfo
+        fields = ('name', 'patronymic', 'surname', 'birthday', 'gender', 'location', 'relocation', 'business_trip')
 
 
 class ContactsForm(forms.ModelForm):
     """Form for processing information about contacts in resume."""
 
-    model = models.Contacts
-    fields = ('phone', 'email', 'telegram')
+    class Meta:
+        model = models.Contacts
+        fields = ('phone', 'email', 'telegram')
 
 
 class PositionForm(forms.ModelForm):
     """Form for processing information about position in resume."""
 
-    model = models.Position
-    fields = ('title', 'salary', 'employment', 'schedule')
+    class Meta:
+        model = models.Position
+        fields = ('title', 'salary', 'employment', 'schedule')
 
 
 class ExperienceForm(forms.ModelForm):
     """Form for processing information about experience in resume."""
 
-    model = models.Experience
-    fields = ('skills', 'about', 'portfolio')
+    class Meta:
+        model = models.Experience
+        fields = ('skills', 'about', 'portfolio')
 
 
 class JobForm(forms.ModelForm):
     """Form for processing information about jobs in resume."""
 
-    model = models.Job
-    fields = ('organization', 'start', 'end', 'location', 'site', 'scope', 'position', 'functions')
+    class Meta:
+        model = models.Job
+        fields = ('organization', 'start', 'end', 'location', 'site', 'scope', 'position', 'functions')
