c38688b8e2a44ae7fe1272e88f0f283f2914ae81
anatolytsi <snipl33t@gmail.com>
2022-01-20 12:53:22 +0100

    vacancies: added employment type to forms

--- hh/vacancies/forms.py
+++ hh/vacancies/forms.py
@@ -6,4 +6,12 @@ from .models import Vacancy
 class VacancyForm(forms.ModelForm):
     class Meta:
         model = Vacancy
-        fields = ('title', 'description', 'hashtags', 'experience', 'salary', 'status')
+        fields = (
+            'title',
+            'description',
+            'hashtags',
+            'employment_type',
+            'experience',
+            'salary',
+            'status'
+        )
