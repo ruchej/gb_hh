2ff7f8d8fd34b9dad40815e996952221bd4fc664
anatolytsi <snipl33t@gmail.com>
2022-01-24 10:46:01 +0100

    accounts: removed unused dependencies

--- hh/accounts/models.py
+++ hh/accounts/models.py
@@ -1,7 +1,5 @@
-import black
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
-from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
 
 
