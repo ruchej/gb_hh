263394c6e8164e592eeca7fd6609fd4a95e8a139
anatolytsi <snipl33t@gmail.com>
2022-01-27 23:46:38 +0100

    updated data generator

--- hh/conf/management/commands/update_db.py
+++ hh/conf/management/commands/update_db.py
@@ -3,7 +3,7 @@ import random
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
 
-from accounts.models import UserStatus
+from accounts.models import UserStatus, Employer, JobSeeker
 from resumes import models as resumes_models
 from vacancies import models as vacancies_models
 from blog import models as blog_models
@@ -21,33 +21,56 @@ class Command(BaseCommand):
         if not User.objects.filter(username='suser').exists():
             User.objects.create_superuser('suser', 'suser@example.local', 'A1234567a')
         if not User.objects.filter(username='employer').exists():
-            User.objects.create_superuser('employer', 'employer@employer.employer', 'A1234567a',
-                                          status=UserStatus.EMPLOYER)
+            employer = User.objects.create_superuser('employer', 'employer@employer.employer', 'A1234567a',
+                                                     status=UserStatus.EMPLOYER)
+            Employer.objects.get(user=employer).delete()
+            mixer.blend(Employer, user=employer, name='Google', description=mixer.RANDOM,
+                        phone=mixer.RANDOM, address=mixer.RANDOM)
+        else:
+            employer = User.objects.get(username='employer')
+
         if not User.objects.filter(username='employee').exists():
-            User.objects.create_superuser('employee', 'employee@employee.employee', 'A1234567a',
-                                          status=UserStatus.JOBSEEKER)
+            employee = User.objects.create_superuser('employee', 'employee@employee.employee', 'A1234567a',
+                                                     status=UserStatus.JOBSEEKER)
+            JobSeeker.objects.get(user=employee).delete()
+            mixer.blend(JobSeeker, user=employee, first_name='Василий', patronymic='Васильевич',
+                        last_name='Пупкин', date_birth=mixer.RANDOM, sex=JobSeeker.Sex.MAN)
+            resume = mixer.blend(resumes_models.Resume, user=employee)
+            mixer.cycle(5).blend(resumes_models.Job, experience=resume.experience)
+        else:
+            employee = User.objects.get(username='employee')
+        mixer.blend(recruiting_models.Response, vacancy__employer=employer, resume__user=employee)
+
+    @staticmethod
+    def create_users():
+        for _ in range(random.randint(10, 20)):
+            user = mixer.blend(User, status=random.choice([UserStatus.JOBSEEKER, UserStatus.EMPLOYER]))
+            if user.status == UserStatus.JOBSEEKER:
+                JobSeeker.objects.get(user=user).delete()
+                mixer.blend(JobSeeker, user=user, date_birth=mixer.RANDOM,
+                            sex=random.choice([JobSeeker.Sex.MAN, JobSeeker.Sex.WOMAN]))
+            elif user.status == UserStatus.EMPLOYER:
+                Employer.objects.get(user=user).delete()
+                mixer.blend(Employer, user=user, name=mixer.RANDOM, description=mixer.RANDOM,
+                            phone=mixer.RANDOM, address=mixer.RANDOM)
 
     @staticmethod
     def create_resumes():
-        employee = User.objects.get(username='employee')
-        mixer.blend(resumes_models.Resume, user=employee)
         for _ in range(random.randint(10, 100)):
-            mixer.blend(resumes_models.Resume)
+            mixer.blend(resumes_models.Resume, user=mixer.SELECT,
+                        user__status=UserStatus.JOBSEEKER)
 
     @staticmethod
     def create_jobs():
         for _ in range(random.randint(10, 100)):
-            mixer.blend(resumes_models.Job)
-        resume = resumes_models.Resume.objects.filter(user__username='employee').first()
-        mixer.blend(resumes_models.Job, experience=resume.experience)
-        mixer.blend(resumes_models.Job, experience=resume.experience)
+            mixer.blend(resumes_models.Job, experience=mixer.SELECT)
 
     @staticmethod
     def create_vacancies():
         for _ in range(random.randint(10, 100)):
-            mixer.blend(vacancies_models.Vacancy)
-        employer = User.objects.get(username='employer')
-        mixer.blend(vacancies_models.Vacancy, employer=employer)
+            mixer.blend(vacancies_models.Vacancy, employer=mixer.SELECT,
+                        employer__status=UserStatus.EMPLOYER, description=mixer.RANDOM,
+                        hashtags=mixer.RANDOM)
 
     @staticmethod
     def create_blog():
@@ -57,15 +80,12 @@ class Command(BaseCommand):
     @staticmethod
     def create_responses():
         for _ in range(random.randint(10, 100)):
-            mixer.blend(recruiting_models.Response)
-        employer = User.objects.get(username='employer')
-        employee = User.objects.get(username='employee')
-        mixer.blend(recruiting_models.Response, vacancy__employer=employer, resume__user=employee)
+            mixer.blend(recruiting_models.Response, vacancy=mixer.SELECT, resume=mixer.SELECT)
 
     @staticmethod
     def create_offers():
         for _ in range(random.randint(10, 100)):
-            mixer.blend(recruiting_models.Offer)
+            mixer.blend(recruiting_models.Offer, vacancy=mixer.SELECT, resume=mixer.SELECT)
 
     @staticmethod
     def clear_db():
@@ -92,11 +112,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if options.get('fill'):
             self.create_suser()
+            self.create_users()
             self.create_resumes()
+            self.create_jobs()
             self.create_vacancies()
             self.create_blog()
             self.create_responses()
             self.create_offers()
-            self.create_jobs()
         elif options.get('clear'):
             self.clear_db()
