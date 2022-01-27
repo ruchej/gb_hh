f5ef1aaa6ba88c3cb8bcc787c01302b0258e5745
Danil Chuzhikov <danil.chuzhikov@gmail.com>
2022-01-27 08:47:04 +0300

    del trash

--- hh/blog/management/commands/fill_news.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from blog.models import Article
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        news = Article.objects.create(title='News1', short_description='qweqwe', description='qweqweqwe',
-                                      author='AuthorName')
