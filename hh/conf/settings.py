d6431aac600545133a2078af4edb3c41750d5a40
anatolytsi <snipl33t@gmail.com>
2022-02-03 22:12:15 +0100

    changed language

--- hh/conf/settings.py
+++ hh/conf/settings.py
@@ -140,6 +140,8 @@ STATICFILES_DIRS = (
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -231,7 +233,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 USE_I18N = True
 USE_L10N = False
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'ru-ru'
+LANGUAGES = (
+    ('ru', 'русский'),
+    ('en', 'english'),
+)
 
 DATE_FORMAT = 'd E Y'
 
