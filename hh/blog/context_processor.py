7cfcd43b589162b49f1a01485615938f417de607
anatolytsi <snipl33t@gmail.com>
2022-02-10 11:03:15 +0100

    blog: added recent news fix

--- hh/blog/context_processor.py
+++ hh/blog/context_processor.py
@@ -2,7 +2,6 @@ from .models import Article
 
 
 def recent_news(request):
-    recent_news = Article.objects.all().order_by('-created_at')[:3]
     return {
-        'recent_news': Article.objects.all().order_by('-created_at')[:3]
+        'recent_news': Article.objects.filter(is_active=True)[:3]
     }
