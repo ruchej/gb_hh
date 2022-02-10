559018452793d0fa6683065b5afe52f25efe1a49
Danil Chuzhikov <danil.chuzhikov@gmail.com>
2022-02-10 18:56:40 +0300

    rss not finally

--- hh/blog/views.py
+++ hh/blog/views.py
@@ -1,5 +1,7 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+import feedparser
 
 from el_pagination.views import AjaxListView
 
@@ -46,3 +48,13 @@ class DetailPost(DetailView):
     slug_url_kwarg = 'post_slug'
     template_name = 'blog/article_detail.html'
     context_object_name = 'post'
+
+
+def rss_parser(request):
+    url = 'https://lenta.ru/rss/top7'
+    feeder = feedparser.parse(url)
+    for entry in feeder['entries']:
+        feed_title = entry.title
+        link = entry.link
+        context = {'feed_title': feed_title, 'link': link, }
+        return render(request, 'snippets/recent_news.html', context)
