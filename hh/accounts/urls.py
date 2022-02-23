03d40bb198b6f7b7a41930cfc2166ecf571af5b9
Danil Chuzhikov <danil.chuzhikov@gmail.com>
2022-02-24 00:08:43 +0300

    add funct change password in detail profile

--- hh/accounts/urls.py
+++ hh/accounts/urls.py
@@ -27,4 +27,11 @@ urlpatterns = [
     path('password_reset_complete/',
          auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
          name='password_reset_complete'),
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(template_name='accounts/password_change_form.html',
+                                               success_url=reverse_lazy('accounts:password_change_done')),
+         name='password_change'),
+    path('password_change_done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
+         name='password_change_done'),
 ]
