64bee5d75c93f2659df11f52e3b3eb3f348e8788
anatolytsi <snipl33t@gmail.com>
2022-02-24 16:09:27 +0100

    blog: increased title length

--- hh/blog/models.py
+++ hh/blog/models.py
@@ -10,7 +10,7 @@ from django.utils.text import slugify
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True,
                             db_index=True, verbose_name='URL', editable=False)
     short_description = models.CharField(max_length=200)
